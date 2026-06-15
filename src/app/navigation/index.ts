1a0445fc10e8e89612e74024284bce58bb6f9109
Nitin <adityagithub311@gmail.com>
2026-06-15 19:24:51 +0530

    check

--- src/app/navigation/index.ts
+++ src/app/navigation/index.ts
@@ -2,6 +2,7 @@ import { dashboards } from "./segments/dashboards";
 import { master } from "./segments/master";
 import { leadmaster } from "./segments/leadmaster";
 
+
 export const navigation = [
   dashboards,
   master,
